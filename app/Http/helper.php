aa816a236b35a56bde7644f7c0a2f19adfeda2ca
Shakeel Ahmad <shakeel2717@gmail.com>
2022-07-28 22:58:05 -0700

    refer count only valid

--- app/Http/helper.php
+++ app/Http/helper.php
@@ -95,3 +95,39 @@ function totalRefersGet($user_id)
 
     return $refers->count() + $inDirectRefers->count();
 }
+
+
+function totalIndirectCount($user_id)
+{
+    $count = 0;
+    $level1Refers = Transaction::where('type', 'reward')->where('user_id', $user_id)->where('note', 'level 1')->get();
+    foreach ($level1Refers as $level1) {
+        // $count++;
+        $level2Refers = Transaction::where('type', 'reward')->where('user_id', $user_id)->where('note', 'level 2')->get();
+        foreach ($level2Refers as $level1) {
+            $count++;
+            $level3Refers = Transaction::where('type', 'reward')->where('user_id', $user_id)->where('note', 'level 3')->get();
+            foreach ($level3Refers as $level1) {
+                $count++;
+                $level4Refers = Transaction::where('type', 'reward')->where('user_id', $user_id)->where('note', 'level 4')->get();
+                foreach ($level4Refers as $level1) {
+                    $count++;
+                    $level5Refers = Transaction::where('type', 'reward')->where('user_id', $user_id)->where('note', 'level 5')->get();
+                    foreach ($level5Refers as $level1) {
+                        $count++;
+                        $level6Refers = Transaction::where('type', 'reward')->where('user_id', $user_id)->where('note', 'level 6')->get();
+                        foreach ($level6Refers as $level1) {
+                            $count++;
+                            $level7Refers = Transaction::where('type', 'reward')->where('user_id', $user_id)->where('note', 'level 7')->get();
+                            foreach ($level7Refers as $level1) {
+                                $count++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return $count;
+}
